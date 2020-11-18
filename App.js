cf2089f21b1ac6b536f22ced0c84a75647ddf9de
Rodrigo Nunes <nunesrodrigo13@outlook.com>
2020-11-18 15:10:27 -0300

    Fix: Removed unused library

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
 
 import React, { useEffect } from 'react';
-import SplashScreen from 'react-native-splash-screen'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppearanceProvider } from 'react-native-appearance';
 
 
 import Main from './src/main';
 export default function App() {
-
-  useEffect(() => {
-    SplashScreen.hide();
-  });
-
   return (
     <SafeAreaProvider>
       <AppearanceProvider>
